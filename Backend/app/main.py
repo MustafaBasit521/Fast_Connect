7346ef93493fe452f69bd094768574104070504c
Muhammad Mustafa Basit <mustafabasit521@gmail.com>
2026-08-07 12:32:21 +0500

    Cloud Db is attached from start and also structurized Backend Folder

---
+++ Backend/app/main.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+
+from fastapi import FastAPI
+
+from app.database.connection import db
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {
+        "message": "FAST Connect Backend is running!"
+    }
+
+
+@app.get("/test-db")
+async def test_database():
+    await db.command("ping")
+
+    return {
+        "message": "MongoDB Atlas connected successfully!"
+    }
+
+
+# @app.post("/test-user")
+# async def create_test_user():
+#     users = db["users"]
+
+#     user = {
+#         "name": "Test User",
+#         "email": "test@fastconnect.com",
+#         "role": "user",
+#         "created_at": datetime.utcnow()
+#     }
+
+#     result = await users.insert_one(user)
+
+#     return {
+#         "message": "User inserted successfully",
+#         "user_id": str(result.inserted_id)
+#     }
+
+# @app.post("/Another-tester")
+# async def create_another_test_User():
+#     users=db["users"]
+#     user={
+#         "name":"Another tester",
+#         "role":"nothing"
+#     }
+#     result=await users.insert_one(user)
+#     return {
+#         "message":"Another tester inserted successfully",
+#         "user_id":str(result.inserted_id)
+#     }
